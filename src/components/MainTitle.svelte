d0548307da69a54e73a860127026f687fa0c6ce6
Kayoshi-dev <maxime.jme@gmail.com>
2023-11-28 23:17:05 +0100

    Improve home page

---
+++ src/components/MainTitle.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { blur } from 'svelte/transition';
+
+	let isHovering = false;
+</script>
+
+<h1
+	class="text-4xl relative h-10 font-bold tracking-tight"
+	on:mouseover={() => (isHovering = true)}
+	on:focus={() => {}}
+	on:mouseleave={() => (isHovering = false)}
+>
+	<a href="https://salitag.vercel.app/"
+		>{#if isHovering}
+			<span transition:blur class="absolute w-full flex justify-center">ᜐᜎᜒᜆᜄ᜴ </span>
+		{:else}
+			<span transition:blur class="absolute w-full flex justify-center">Salitag</span>
+		{/if}
+	</a>
+</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Subtitle from '../components/Subtitle.svelte';
+	import MainTitle from '../components/MainTitle.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -9,15 +9,23 @@
 	<meta property="og:image" content={`${data.originOG}?word=${data.word.word}`} />
 </svelte:head>
 
-<div class="min-h-screen min-w-screen flex flex-col justify-center items-center antialiased">
-	<h1 class="text-4xl">Salitag</h1>
-	<Subtitle />
+<div
+	class="h-screen min-w-screen flex flex-col justify-between items-center antialiased bg-slate-100 dark:bg-gradient-to-b dark:from-gray-900 dark:to-gray-800"
+>
+	<nav>
+		<MainTitle />
+		<h2 class="text-gray-800 dark:text-white italic">One tagalog word per day</h2>
+	</nav>
 
-	<h1
-		class="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 capitalize"
-	>
-		{data.word.word}
-	</h1>
-	<h2>{data.word.definition}</h2>
-	<h3>{data.word.type}</h3>
+	<main class="flex flex-1 flex-col justify-center">
+		<h1
+			class="font-extrabold text-transparent leading-normal md:leading-relaxed text-6xl md:text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 dark:text-white capitalize"
+		>
+			{data.word.word}
+		</h1>
+		<div class="flex flex-col items-center">
+			<h2>{data.word.definition}</h2>
+			<i>{data.word.type}</i>
+		</div>
+	</main>
 </div>
