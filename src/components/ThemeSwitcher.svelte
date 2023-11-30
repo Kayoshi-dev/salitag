34d458da35e82623d0fd5b00ddc4abf5edc15e33
Kayoshi-dev <maxime.jme@gmail.com>
2023-11-30 21:37:42 +0100

    Get rid of browser defaults select (Safari)

--- src/components/ThemeSwitcher.svelte
+++ src/components/ThemeSwitcher.svelte
@@ -4,7 +4,7 @@
 
 <select
 	bind:value={$theme}
-	class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+	class="appearance-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 >
 	<option value="system">System</option>
 	<option value="light">Light</option>
